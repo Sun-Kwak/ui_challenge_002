0462b5e667a3490f641553c15428848d184cf374
Sunlution <sunface@outlook.kr>
2024-04-20 13:09:23 +0900

    코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -105,33 +105,34 @@ class _PushableButtonPageState extends State<PushableButtonPage> {
 /// A widget to show a "3D" pushable button
 class PushableButton extends StatefulWidget {
   const PushableButton({
-    super.key,
+    Key? key,
     this.child,
     required this.hslColor,
     required this.height,
     this.elevation = 8.0,
     this.shadow,
     this.onPressed,
-  }) : assert(height > 0);
+  })  : assert(height > 0),
+        super(key: key);
 
-  /// child widget (normally a Text or Icon)
+  /// Child widget (normally a Text or Icon)
   final Widget? child;
 
   /// Color of the top layer
   /// The color of the bottom layer is derived by decreasing the luminosity by 0.15
   final HSLColor hslColor;
 
-  /// height of the top layer
+  /// Height of the top layer
   final double height;
 
-  /// elevation or "gap" between the top and bottom layer
+  /// Elevation or "gap" between the top and bottom layer
   final double elevation;
 
   /// An optional shadow to make the button look better
   /// This is added to the bottom layer only
   final BoxShadow? shadow;
 
-  /// button pressed callback
+  /// Button pressed callback
   final VoidCallback? onPressed;
 
   @override
@@ -140,80 +141,102 @@ class PushableButton extends StatefulWidget {
 
 class _PushableButtonState extends State<PushableButton>
     with SingleTickerProviderStateMixin {
-  late double _elevation;
   late AnimationController _controller;
-  late Animation<double> animation;
+  late Animation<double> _animation;
 
   @override
   void initState() {
-    _elevation = widget.elevation;
+    super.initState();
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 100),
     );
-    animation =
-    Tween<double>(begin: 0, end: 1).animate(_controller)
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
       ..addListener(() {
         setState(() {});
       });
-    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTapDown(_) {
+    _controller.forward(from: 0);
+  }
+
+  void _handleTapUp(_) {
+    _controller.reverse(from: 1);
+    if (widget.onPressed != null) {
+      widget.onPressed!();
+    }
+  }
+
+  void _handleTapCancel() {
+    _controller.reverse(from: 1);
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: replace with custom button UI and animations
-    return AnimatedBuilder(animation: animation, builder: (context, child) {
-      final top = animation.value*widget.elevation;
-      return GestureDetector(
-        onTap: widget.onPressed,
-        onTapDown: (_) =>
-            setState(() {
-              _controller.forward(from: 0);
-            }),
-        onTapUp: (_) =>
-            setState(() {
-              _controller.reverse(from: 1);
-            }),
-        child: LayoutBuilder(
-          builder: (context,constraint){
-            return SizedBox(
-              height: widget.height + widget.elevation,
-              width: constraint.maxWidth,
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    top: widget.elevation,
-                    child: Container(
-                      height: widget.height,
-                      decoration: BoxDecoration(
-                        color: widget.hslColor.withLightness(
-                          (widget.hslColor.lightness - 0.15).clamp(0.0, 1.0),
-                        ).toColor(),
-                        borderRadius: BorderRadius.circular(widget.height / 2),
-                        boxShadow: widget.shadow != null
-                            ? [widget.shadow!]
-                            : null,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: top,
-                    child: Container(
-                      height: widget.height,
-                      width: constraint.maxWidth,
-                      decoration: BoxDecoration(
-                        color: widget.hslColor.toColor(),
-                        borderRadius: BorderRadius.circular(widget.height / 2),
-                      ),
-                      child: Center(child: widget.child),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
+    final top = _animation.value * widget.elevation;
+
+    return GestureDetector(
+      onTap: widget.onPressed,
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      child: _buildButtonStack(top),
+    );
+  }
+
+  Widget _buildButtonStack(double top) {
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return SizedBox(
+          height: widget.height + widget.elevation,
+          width: constraint.maxWidth,
+          child: Stack(
+            children: [
+              _buildBottomLayer(),
+              _buildTopLayer(top, constraint),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildBottomLayer() {
+    return Positioned.fill(
+      top: widget.elevation,
+      child: Container(
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: widget.hslColor
+              .withLightness((widget.hslColor.lightness - 0.15).clamp(0.0, 1.0))
+              .toColor(),
+          borderRadius: BorderRadius.circular(widget.height / 2),
+          boxShadow: widget.shadow != null ? [widget.shadow!] : null,
         ),
-      );
-    });
+      ),
+    );
+  }
+
+  Widget _buildTopLayer(double top, BoxConstraints constraint) {
+    return Positioned(
+      top: top,
+      child: Container(
+        height: widget.height,
+        width: constraint.maxWidth,
+        decoration: BoxDecoration(
+          color: widget.hslColor.toColor(),
+          borderRadius: BorderRadius.circular(widget.height / 2),
+        ),
+        child: Center(child: widget.child),
+      ),
+    );
   }
 }
+
